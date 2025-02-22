205d81a947f52d8c3fc9764909306e205fa85f9d
Sargis <terbalyantss@gmail.com>
2025-02-22 04:15:11 +0400

    Fixed sudoku numbers generation and cover algorithm

--- src/Components/Grid/Grid.tsx
+++ src/Components/Grid/Grid.tsx
@@ -1,28 +1,45 @@
 import React, { ReactElement } from "react";
 import "./Grid.scss";
-import { generateRandomSudokuNumbers } from "../../utils/generateSudokuNumbers";
+import { generateRandomSudokuNumbers } from "../../utils/generateHorizontalSudokuLine";
+import { FaEraser, FaPen } from "react-icons/fa";
 
 const Grid = () => {
     const renderGrid = () => {
         const grid: ReactElement[] = [];
         const numbers: number[][] = generateRandomSudokuNumbers();
-        console.log(numbers);
         const flatNumbers: number[] = numbers.flat();
 
         for (let i = 0; i < 81; i++) {
+            const isCovered = Math.random() > 0.5;
+
             grid.push(
                 <div
-                    className="grid-item"
+                    className={`grid-item ${isCovered ? "covered" : ""}`}
                     key={i}
                 >
-                    {flatNumbers[i]}
+                    {isCovered ? "" : flatNumbers[i]}
                 </div>
             );
         }
 
         return grid;
     };
-    return <div className="grid">{renderGrid()}</div>;
+
+    return (
+        <div className="grid-container">
+            <div className="grid">{renderGrid()}</div>
+            <aside>
+                <div>
+                    {FaPen({})}
+                    <span>Pen</span>
+                </div>
+                <div>
+                    {FaEraser({})}
+                    <span>Eraser</span>
+                </div>
+            </aside>
+        </div>
+    );
 };
 
 export default Grid;
--- src/utils/generateHorizontalSudokuLine.ts
+++ src/utils/generateHorizontalSudokuLine.ts
@@ -1,10 +1,10 @@
-import { generateRandomNumbersArray } from "./generateRandomNumbersArray";
+import { generateHorizontalSudokuLine } from "./generateRandomNumbersArray";
 
 export const generateRandomSudokuNumbers = () => {
     let sudokuNumbers: number[][] = [[], [], [], [], [], [], [], [], []];
 
     for (let i = 0; i < 9; i++) {
-        const row: number[] = generateRandomNumbersArray(sudokuNumbers, i);
+        const row: number[] = generateHorizontalSudokuLine(sudokuNumbers, i);
         sudokuNumbers[i] = row;
     }
     return sudokuNumbers;
